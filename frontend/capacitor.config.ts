47bb46e8baebf6ef74f201d86202cc08ebeb7ebb
tomcaiyq <1348870256@qq.com>
2026-05-31 01:53:30 +0800

    fix: use capacitor.config.json instead of .ts

--- frontend/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.musicplayer.app',
-  appName: '云音乐',
-  webDir: 'dist'
-};
-
-export default config;
